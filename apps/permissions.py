c9b037fe1d301e07a56446a28ce9e7bee178746f
admin <admin00@gmail.com>
2026-04-07 12:32:18 +0500

    update code

--- apps/permissions.py
+++ apps/permissions.py
@@ -15,22 +15,18 @@
 #         return obj.author == request.user
 #
 
+
 from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        # Tizimga kirmagan foydalanuvchilar faqat o'qiy oladi (GET)
         if request.method in permissions.SAFE_METHODS:
             return True
-        # Yaratish (POST) uchun foydalanuvchi tizimga kirgan bo'lishi shart
         return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        # SAFE_METHODS (GET, HEAD, OPTIONS) hamma uchun ochiq
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        # O'zgartirish va o'chirish faqat muallifga ruxsat
         return obj.author == request.user
